a448c81767f6473b9af5f0bd98abdca32cd25228
Sabah <sabah1994786@gmail.com>
2022-10-23 00:16:42 +0100

    Updated TreeSitter code movements with consistent keybindings

--- nvim/lua/plug-config/treeSitter.lua
+++ nvim/lua/plug-config/treeSitter.lua
@@ -24,12 +24,9 @@ require 'nvim-treesitter.configs'.setup {
     textobjects = {
         select = {
             enable = true,
-
             -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
-
             keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
                 ["aC"] = "@class.outer",
                 ["iC"] = "@class.inner",
                 ["af"] = "@function.outer",
@@ -45,20 +42,16 @@ require 'nvim-treesitter.configs'.setup {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
+                ["]C"] = "@class.outer",
+                ["]f"] = "@function.outer",
+                ["]l"] = "@loop.outer",
+                ["]c"] = "@conditional.outer",
             },
             goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
+                ["[C"] = "@class.outer",
+                ["[f"] = "@function.outer",
+                ["[l"] = "@loop.outer",
+                ["[c"] = "@conditional.outer",
             },
         },
     },
