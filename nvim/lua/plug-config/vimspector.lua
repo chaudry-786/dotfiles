ea96ac6ad65bff27fd565411b6d23b938591fd1a
Sabah <sabah1994786@gmail.com>
2022-12-17 02:13:46 +0000

    Added debug configs for python and changed mapping

--- nvim/lua/plug-config/vimspector.lua
+++ nvim/lua/plug-config/vimspector.lua
@@ -12,6 +12,8 @@ function ToggleDebugMode()
         for key in pairs(keysToUnmap) do
             del_unmap("n", keysToUnmap[key])
         end
+        keymap("n", "<leader>d", ":bdelete<CR>", opts)
+        vim.cmd("call vimspector#Reset()")
         print("DebugMode is OFF")
     else
         keymap("n", "<Leader>ds", ":call vimspector#Launch()<CR>", opts)
@@ -28,9 +30,55 @@ function ToggleDebugMode()
         keymap("n", "<Right>", "<Plug>VimspectorStepInto", {})
         keymap("n", "<Down>", "<Plug>VimspectorStepOver", {})
 
+        del_unmap("n", "<leader>d")
+        vim.cmd("call vimspector#Launch()")
         print("DebugMode is ON")
     end
     DebugMode = not DebugMode
 end
 
-keymap("n", "<leader>td", ":lua ToggleDebugMode()<CR>", opts)
+keymap("n", "<F5>", ":lua ToggleDebugMode()<CR>", opts)
+
+-- reference https://puremourning.github.io/vimspector/configuration.html#predefined-variables
+-- TODO: dynamically create debug configs, reduce repeated lines
+local breakPointDict = {
+    exception = {
+        raised = "N",
+        uncaught = "",
+        userUnhandled = ""
+    }
+}
+vim.g.vimspector_configurations = {
+    CurrentFile = {
+        adapter = "debugpy",
+        filetypes = { "python" },
+        configuration = {
+            args = { "*${args}" },
+            cwd = "${workspaceRoot}",
+            program = "${file}",
+            python = "${workspaceRoot}/venv/bin/python",
+            request = "launch",
+            stopOnEntry = false,
+            type = "python",
+        },
+        breakpoints = breakPointDict
+    },
+
+    Flask = {
+        adapter = "debugpy",
+        filetypes = { "python" },
+        configuration = {
+            args = { "run" },
+            cwd = "${workspaceRoot}",
+            debugOptions = {},
+            env = { FLASK_APP = "app.py" },
+            jinja = true,
+            program = "${workspaceRoot}/venv/bin/flask",
+            python = "${workspaceRoot}/venv/bin/python",
+            request = "launch",
+            stopOnEntry = false,
+            type = "python",
+        },
+        breakpoints = breakPointDict
+    }
+}
