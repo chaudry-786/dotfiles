538f1d732c90a6169a2d6f33f73109bf5c5b01b4
sabah <sabah1994786@gmail.com>
2022-12-21 15:21:18 +0000

    telescope; if more than one item selected, send to qflist

--- nvim/lua/plug-config/telescope.lua
+++ nvim/lua/plug-config/telescope.lua
@@ -1,7 +1,22 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-local actions = require("telescope.actions")
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+local custom_actions = {}
+-- if more than one items selected, secnd to qf list
+function custom_actions.fzf_multi_select(prompt_bufnr)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local num_selections = table.getn(picker:get_multi_selection())
+    if num_selections > 1 then
+        actions.send_to_qflist(prompt_bufnr)
+        actions.open_qflist(prompt_bufnr)
+    else
+        actions.file_edit(prompt_bufnr)
+    end
+end
+
 require('telescope').setup({
     defaults = {
         mappings = {
@@ -9,6 +24,7 @@ require('telescope').setup({
                 ["<esc>"] = actions.close,
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
+                ['<CR>'] = custom_actions.fzf_multi_select
             },
         },
         prompt_prefix = "   ",
