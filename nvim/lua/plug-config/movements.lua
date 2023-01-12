335aca20de9d371d698331a1f91f7fa44bdd3ee9
sabah <sabah1994786@gmail.com>
2023-01-12 16:42:17 +0000

    changed leap mappings

--- nvim/lua/plug-config/movements.lua
+++ nvim/lua/plug-config/movements.lua
@@ -2,8 +2,8 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 require("leap")
-keymap("", "f", "<Plug>(leap-forward-to)", { silent = true })
-keymap("", "F", "<Plug>(leap-backward-to)", { silent = true })
+keymap("", "s", "<Plug>(leap-forward-to)", { silent = true })
+keymap("", "S", "<Plug>(leap-backward-to)", { silent = true })
 
 -- easy linewise movement
 require "hop".setup()
