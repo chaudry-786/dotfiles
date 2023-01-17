fe3d52df383a61c4ba30c66d90aa05af6e01de84
sabah <sabah1994786@gmail.com>
2023-01-17 12:38:45 +0000

    tmux and nvimtree changes

--- nvim/lua/plug-config/nvimTree.lua
+++ nvim/lua/plug-config/nvimTree.lua
@@ -61,3 +61,9 @@ vim.api.nvim_create_autocmd("BufEnter", {
         end
     end
 })
+
+-- automatically open newly created file
+local api = require("nvim-tree.api")
+api.events.subscribe(api.events.Event.FileCreated, function(file)
+  vim.cmd("edit " .. file.fname)
+end)
