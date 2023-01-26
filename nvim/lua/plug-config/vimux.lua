4dfae1927f0cfb373fefd3c5d88a5dcdbe65030b
sabah <sabah1994786@gmail.com>
2023-01-26 09:43:54 +0000

    minor changes related to tmux

--- nvim/lua/plug-config/vimux.lua
+++ nvim/lua/plug-config/vimux.lua
@@ -3,6 +3,9 @@ vim.api.nvim_create_autocmd("FileType",
         command = [[noremap <silent><buffer> <Leader>x :call VimuxRunCommand("clear; src; python " . bufname("%"))<CR>]] })
 -- do not send <CR>
 -- command = [[noremap <silent><buffer> <Leader>x :call VimuxRunCommand("clear; src; python " . bufname("%"), 0)<CR>]] })
+vim.api.nvim_create_autocmd("FileType",
+    { group = "CustomAutoCmds", pattern = { "c" },
+        command = [[noremap <silent><buffer> <Leader>x :call VimuxRunCommand("clear; gcc " . bufname("%") . " -o " . expand("%:t:r") . " && ./" . expand("%:t:r") )<CR>]] })
 
 vim.g.VimuxHeight = 30
 vim.g.VimuxOrientation = "h"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,7 +3,7 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-plugins=(sudo git web-search history zsh-autosuggestions)
+plugins=(sudo git web-search history zsh-autosuggestions tmux)
 
 source $ZSH/oh-my-zsh.sh
 
