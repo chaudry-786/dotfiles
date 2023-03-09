8054b7e2fbf456326846cfb8263f50d3d512d37c
Sabah Uddin <sabah1994786@gmail.com>
2023-03-09 17:12:49 +0000

    updated auto qf list update logic

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -42,25 +42,26 @@ vim.api.nvim_create_autocmd("VimEnter",
 
 
 vim.api.nvim_create_augroup("QfAutoCmds", { clear = true })
--- automatiaclly, update, open and close qf on a given pattern for current buffer.
+-- automatiaclly, update qf on a given pattern for current buffer.
 local function create_qf_autocmds(qf_patterns)
 
     local c_autocmd = vim.api.nvim_create_autocmd
     for file_type, pattern in pairs(qf_patterns) do
-        -- wincmd p = go to previous window (don't focus on qf list)
-        c_autocmd("BufWinEnter",
-            { group = "QfAutoCmds", pattern = file_type, command = "silent vimgrep/" ..
-                pattern .. " % | copen | wincmd p" })
-         -- j = do not jump
-        c_autocmd("BufWritePost",
-            { group = "QfAutoCmds", pattern = file_type, command = "silent vimgrep/" .. pattern .. "j %" })
-        c_autocmd("BufWinLeave",
-            { group = "QfAutoCmds", pattern = file_type, command = "cclose" })
+        c_autocmd({ "BufWinEnter", "BufWritePost" },
+            { group = "QfAutoCmds", pattern = file_type,
+                callback = function()
+                    if vim.fn.match(vim.fn.getline(1, "$"), pattern) ~= -1 then
+                        vim.cmd("silent vimgrep/" .. pattern .. "/j %")
+                    else
+                        -- clean qflist
+                        vim.fn.setqflist({})
+                    end
+                end })
     end
 end
 
-local qf_patterns = { ["*.sql"] = [[\v(\a|_)+ AS \(/]] }
--- create_qf_autocmds(qf_patterns)
+local qf_patterns = { ["*.sql"] = [[\v(\a|_)+ AS \(]] }
+create_qf_autocmds(qf_patterns)
 
 -- ignore quick fix list from buflist
 vim.api.nvim_create_autocmd("FileType", { group = "CustomAutoCmds", pattern = 'qf', command = [[ set nobuflisted ]] })
