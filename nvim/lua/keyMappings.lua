f46ca0a79e80b3643966ac6b484e6a2861f1d662
sabah <sabah1994786@gmail.com>
2023-08-05 12:19:53 +0100

    added consistent movement for folds

--- nvim/lua/keyMappings.lua
+++ nvim/lua/keyMappings.lua
@@ -208,6 +208,10 @@ keymap("n", "]q", ":cnext<CR>", opts)
 keymap("n", "[Q", ":cfirst<CR>", opts)
 keymap("n", "]Q", ":clast<CR>", opts)
 
+--fold movement
+keymap("", "[z", "zk", opts)
+keymap("", "]z", "zj", opts)
+
 -- to make sure indent lines update
 for _, keymap in pairs({ "zo", "zO", "zc", "zC", "za", "zA", "zv", "zx", "zX", "zm", "zM", "zr", "zR", }) do
     vim.api.nvim_set_keymap("n", keymap, keymap .. "<CMD>IndentBlanklineRefresh<CR>", { noremap = true, silent = true })
