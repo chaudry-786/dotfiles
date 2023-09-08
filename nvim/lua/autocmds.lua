b8d85f29fa0adcb5286555e775bb373df2c17532
sabah <sabah1994786@gmail.com>
2023-09-08 22:17:34 +0100

    automatic python fstring

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -72,3 +72,29 @@ create_qf_autocmds(qf_patterns)
 
 -- ignore quick fix list from buflist
 vim.api.nvim_create_autocmd("FileType", { group = "CustomAutoCmds", pattern = "qf", command = [[ set nobuflisted ]] })
+
+-- Treesitter automatic Python format strings
+vim.api.nvim_create_autocmd("InsertCharPre", {
+    pattern = { "*.py" },
+    group = "CustomAutoCmds",
+    callback = function(opts)
+        -- Only run if f-string escape character is typed
+        if vim.v.char ~= "{" then return end
+
+        -- Get node and return early if not in a string
+        local node = vim.treesitter.get_node()
+
+        if not node then return end
+        if node:type() ~= "string" then node = node:parent() end
+        if not node or node:type() ~= "string" then return end
+
+        local row, col, _, _ = vim.treesitter.get_node_range(node)
+
+        -- Return early if string is already a format string
+        local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
+        if first_char == "f" then return end
+
+        -- Otherwise, make the string a format string
+        vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<Esc>`'la")
+    end,
+})
