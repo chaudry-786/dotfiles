3519a7ffb635919d6ee5fa149c3f412231a2ea81
sabah <sabah1994786@gmail.com>
2023-10-02 23:55:50 +0100

    folds text is now syntax highlighted

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -40,25 +40,13 @@ set.dir = os.getenv("HOME") .. "/tmp"                   --swap file directory
 
 -- fold
 set.foldmethod = "expr"                                 --treesitter for folds
-set.foldexpr = "nvim_treesitter#foldexpr()"             --treesitter for folds
+set.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+set.foldtext = "v:lua.vim.treesitter.foldtext()"
 vim.api.nvim_create_autocmd("FileType",
     { group = "CustomAutoCmds", pattern = "sql", command = [[ setlocal foldmethod=indent ]] })
 set.foldenable = false                                  --do not auto create folds when file opens
 set.foldnestmax = 3                                     --max nested fold level
 set.foldlevel = 2                                       --fold level: zr or zm
-function _G.MyFoldText()
-    local foldStartText = vim.fn.getline(vim.v.foldstart) .. "  "
-    local foldLineCount = vim.v.foldend - vim.v.foldstart
-    local totalLines    = vim.api.nvim_buf_line_count(0)
-    local foldPercInBuf = math.floor((foldLineCount / totalLines) * 100)
-    local conLen = string.len(foldStartText .. foldLineCount .. " lines: ")
-    local winWidth = vim.api.nvim_win_get_width(0)
-
-    return foldStartText ..
-        string.rep("-", ((winWidth - conLen) - 25)) ..
-        "  " .. foldLineCount .. " lines: " .. string.rep(" ", 2 - string.len(tostring(foldPercInBuf))) .. foldPercInBuf .. "%"
-end
-set.foldtext      = "v:lua.MyFoldText()"
 set.fillchars = { fold = " ", foldopen = "", foldclose = "", foldsep = " " }
 set.foldcolumn = "1"
 
