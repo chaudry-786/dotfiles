e75cd561b5e0df42ec7a494c461ce35ee4a9eccf
sabah <sabah1994786@gmail.com>
2023-10-04 23:18:07 +0100

    updated fold text

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -41,13 +41,25 @@ set.dir = os.getenv("HOME") .. "/tmp"                   --swap file directory
 -- fold
 set.foldmethod = "expr"                                 --treesitter for folds
 set.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-set.foldtext = "v:lua.vim.treesitter.foldtext()"
+-- set.foldtext = "v:lua.vim.treesitter.foldtext()"
+function Foldtext()
+    local text = vim.treesitter.foldtext()
+    local n_lines = vim.v.foldend - vim.v.foldstart
+    local text_lines = " lines"
+    if n_lines == 1 then
+        text_lines = " line"
+    end
+    table.insert(text, { " + " .. n_lines .. text_lines, { "Folded" } })
+    return text
+end
+vim.opt.foldtext = "v:lua.Foldtext()"
+
 vim.api.nvim_create_autocmd("FileType",
     { group = "CustomAutoCmds", pattern = "sql", command = [[ setlocal foldmethod=indent ]] })
 set.foldenable = false                                  --do not auto create folds when file opens
 set.foldnestmax = 3                                     --max nested fold level
 set.foldlevel = 2                                       --fold level: zr or zm
-set.fillchars = { fold = " ", foldopen = "", foldclose = "", foldsep = " " }
+set.fillchars = { fold = " ", foldopen = "", foldclose = "", foldsep = " " }
 set.foldcolumn = "1"
 
 --4 spaces with tab
