f91ce009535d7259b60664ac93233ddf9f0e31cc
sabah <sabah1994786@gmail.com>
2023-10-22 12:11:23 +0100

    enhanced vimgrep keymapping to operate over current file extenions or mulitple file extensions

--- nvim/lua/keyMappings.lua
+++ nvim/lua/keyMappings.lua
@@ -200,12 +200,25 @@ keymap("n", "/", [[/\v]], { noremap = true })
 keymap("n", "?", [[?\v]], { noremap = true })
 local function enable_very_magic()
     local cmdline, cmdtype = vim.fn.getcmdline(), vim.fn.getcmdtype()
+    if cmdtype ~= ":" then
+        return "/"
+    end
     -- list of valid command-line inputs that trigger very magic mode
     local valid_values = { "s", [['<,'>s]], "%s", "g", [['<,'>g]], "g!", [['<,'>g!]] }
-    if cmdtype == ":" and vim.tbl_contains(valid_values, cmdline) then
+    if vim.tbl_contains(valid_values, cmdline) then
         return "/\\v"
-    elseif cmdtype == ":" and cmdline == "v" then
+    elseif cmdline == "v" then
         return [[imgrep /\v/ **/*]] .. string.rep("<Left>", 6)
+    elseif cmdline == "vext" then
+        -- for "vext/" command, grep over current file extension
+        local current_extension = vim.fn.expand("%:e")
+        vim.fn.setcmdline("vimgrep /\\v/ **/*." .. current_extension, 12)
+        return
+    elseif string.match(cmdline, "^vext:") then
+        -- for "vext:ext1,ext2,ext3/" command, grep over specified extensions
+        local extensions = string.sub(cmdline, 6, -1) -- extract extensions from the command
+        vim.fn.setcmdline("vimgrep /\\v/ **/*.{" .. extensions .. ",}", 12)
+        return
     end
     return "/"
 end
