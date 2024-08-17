dd6ab85e5779ffd87c9f31e5c3897159da59c9e8
Sabah Din <sabah1994786@outlook.com>
2024-08-17 21:03:53 +0100

    create directory if it doesn't exist

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -64,6 +64,7 @@ install_and_setup_tmux () {
 install_and_setup_kitty () {
     # install kitty
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+    mkdir -p ~/.local/bin
     ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
     cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
     sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
