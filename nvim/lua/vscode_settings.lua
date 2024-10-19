6972ff03314a2750321f8ae3cc523f6e66f0e5db
sabah uddin <sabah1994786@gmail.com>
2024-10-19 02:04:47 +0100

    before going to diff mode, make sure single layout is enabled

--- nvim/lua/vscode_settings.lua
+++ nvim/lua/vscode_settings.lua
@@ -278,9 +278,18 @@ vim.cmd("nnoremap <silent> <leader>ga :call VSCodeNotify('git.stage')<CR>")
 vim.cmd("nnoremap <silent> <leader>gb :call VSCodeNotify('gitlens.toggleLineBlame')<CR>")
 vim.cmd("nnoremap <silent> <leader>gB :call VSCodeNotify('gitlens.toggleFileBlame')<CR>")
 vim.cmd("nnoremap <silent> <leader>gc :call VSCodeNotify('git.clean')<CR>")
-vim.cmd("nnoremap <silent> <leader>gd :call VSCodeNotify('git.openChange')<CR>")
+-- workbench.action.editorLayoutSingle
+keymap("n", "<leader>gd", function()
+    Vscode.action("workbench.action.editorLayoutSingle")
+    os.execute("sleep 0.2")
+    Vscode.action("git.openChange")
+end, opts)
 -- Merge conflict editor
-vim.cmd("nnoremap <silent> <leader>gD :call VSCodeNotify('merge-conflict.compare')<CR>")
+keymap("n", "<leader>gD", function()
+    Vscode.action("workbench.action.editorLayoutSingle")
+    os.execute("sleep 0.2")
+    Vscode.action("merge-conflict.compare")
+end, opts)
 
 ------------------------------------------------------------------------------
 -- Snippets
