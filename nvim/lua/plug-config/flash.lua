9087af04f01fbb2949105ba71d9f8e7a86c1ef8c
sabah uddin <sabah1994786@gmail.com>
2024-11-06 00:51:18 +0000

    added gl to go to line

--- nvim/lua/plug-config/flash.lua
+++ nvim/lua/plug-config/flash.lua
@@ -7,14 +7,14 @@ require("flash").setup({
     }
 })
 local flash = require("flash")
-map({"n", "x", "o"}, "s", function()
+map({ "n", "x", "o" }, "s", function()
     flash.jump({
         search = {
             multi_window = false
         }
     })
 end, "Flash: Jump")
-map({"n", "x", "o"}, "gs", function()
+map({ "n", "x", "o" }, "gs", function()
     flash.treesitter_search({
         label = {
             rainbow = {
@@ -31,7 +31,7 @@ map({"n", "x", "o"}, "gs", function()
         }
     })
 end, "Flash Treesitter: Search")
-map({"n", "x", "o"}, "gS", function()
+map({ "n", "x", "o" }, "gS", function()
     flash.treesitter({
         label = {
             rainbow = {
@@ -45,7 +45,7 @@ map({"n", "x", "o"}, "gS", function()
         }
     })
 end, "Flash Treesitter")
-map({"o", "x", "n"}, "<CR>", function()
+map({ "o", "x", "n" }, "gl", function()
     flash.jump({
         search = {
             mode = "search",
@@ -53,7 +53,7 @@ map({"o", "x", "n"}, "<CR>", function()
             multi_window = false
         },
         label = {
-            after = {0, 0}
+            after = { 0, 0 }
         },
         pattern = "^"
     })
