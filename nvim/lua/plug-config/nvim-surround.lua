bc7cea9a342dcfd793493d642ba04f61a9e83d20
Sabah Din <sabah1994786@outlook.com>
2026-09-23 15:46:40 +0100

    fixed surround mappings on new version

--- nvim/lua/plug-config/nvim-surround.lua
+++ nvim/lua/plug-config/nvim-surround.lua
@@ -1,19 +1,7 @@
-require("nvim-surround").setup({
-    keymaps = {
-        insert = false,
-        insert_line = false,
-        normal = "yS",
-        normal_line = false,
-        normal_cur = false,
-        normal_cur_line = false,
-        visual = "<leader>S",
-        visual_line = false,
-        delete = "dS",
-        change = "cS",
-    }
-})
+require("nvim-surround").setup({})
 
 e_map({ "n" }, "yS", [[<Plug>(nvim-surround-normal)]], "Surround text in normal mode.")
 e_map({ "v" }, "S", [[<Plug>(nvim-surround-visual)]], "Surround selected text in visual mode.")
+e_map({ "v" }, "<leader>S", [[<Plug>(nvim-surround-visual)]], "Surround selected text in visual mode.")
 e_map({ "n" }, "dS", [[<Plug>(nvim-surround-delete)]], "Delete surrounding characters.")
 e_map({ "n" }, "cS", [[<Plug>(nvim-surround-change)]], "Change surrounding characters.")
